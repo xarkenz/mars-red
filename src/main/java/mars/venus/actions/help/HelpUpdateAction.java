25fc93e62a7ef0f01bbcfa5ef35f3ae039845413
xarkenz <seanedwardsclarke@gmail.com>
2024-04-24 16:13:00 -0500

    improved error handling in update action

--- src/main/java/mars/venus/actions/help/HelpUpdateAction.java
+++ src/main/java/mars/venus/actions/help/HelpUpdateAction.java
@@ -18,9 +18,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,6 +28,7 @@ import java.util.Map;
  */
 public class HelpUpdateAction extends VenusAction {
     private static final String RELEASES_URL = "https://api.github.com/repos/xarkenz/mars-red/releases";
+    private static final Duration TIMEOUT_DURATION = Duration.of(5, ChronoUnit.SECONDS);
 
     public HelpUpdateAction(VenusUI gui, String name, Icon icon, String description, Integer mnemonic, KeyStroke accel) {
         super(gui, name, icon, description, mnemonic, accel);
@@ -37,66 +36,114 @@ public class HelpUpdateAction extends VenusAction {
 
     @Override
     public void actionPerformed(ActionEvent event) {
+        Map<Version, String> versionsToUrls;
         try {
-            HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(RELEASES_URL))
-                .timeout(Duration.of(5, ChronoUnit.SECONDS))
-                .GET()
-                .build();
-            HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-            JSONArray releaseArray = new JSONArray(response.body());
-
-            // Parse releases into versions and URLs from json response
-            Map<Version, String> versionsToUrls = new HashMap<>();
-            for (int index = 0; index < releaseArray.length(); index++) {
-                JSONObject release = releaseArray.getJSONObject(index);
-                String releaseVersion = release.getString("tag_name");
-                // Remove leading 'v' before tag name
-                if (releaseVersion.startsWith("v")) {
-                    releaseVersion = releaseVersion.replaceFirst("v", "");
-                }
-                versionsToUrls.put(Version.parse(releaseVersion), release.getString("html_url"));
-            }
-            // Sort version entries in decreasing order
-            List<Map.Entry<Version, String>> sortedVersions = versionsToUrls.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
-                .toList();
-
-            // Check if largest fetched version is "greater than" current version
-            Version currentVersion = Version.parse(Application.VERSION);
-            if (!sortedVersions.isEmpty() && sortedVersions.get(0).getKey().compareTo(currentVersion) > 0) {
-                Map.Entry<Version, String> newestVersion = sortedVersions.get(0);
-                int choice = JOptionPane.showConfirmDialog(
-                    this.gui,
-                    "A newer version of " + Application.NAME + " is available.\n"
-                        + "Current version: " + currentVersion + "\n"
-                        + "Latest version: " + newestVersion.getKey() + "\n"
-                        + "Go to release page?",
-                    "Check for Updates",
-                    JOptionPane.YES_NO_OPTION
-                );
-                if (choice == JOptionPane.YES_OPTION) {
-                    Desktop.getDesktop().browse(new URI(newestVersion.getValue()));
-                }
-            }
-            else {
-                JOptionPane.showMessageDialog(
-                    this.gui,
-                    "You are using the latest version of " + Application.NAME + " (" + currentVersion + ").",
-                    "Check for Updates",
-                    JOptionPane.INFORMATION_MESSAGE
-                );
-            }
+            versionsToUrls = fetchReleases();
         }
         catch (IOException exception) {
-            System.err.println(this.getClass().getSimpleName() + ": error 504: could not open releases page");
-        }
-        catch (URISyntaxException exception) {
-            System.err.println(this.getClass().getSimpleName() + ": error 503: could not open releases page");
+            System.err.println(this.getClass().getSimpleName() + ": failed to fetch releases:");
+            exception.printStackTrace();
+            JOptionPane.showMessageDialog(
+                this.gui,
+                "Failed to fetch release information from GitHub. (Are you connected to the internet?)",
+                "Error",
+                JOptionPane.ERROR_MESSAGE
+            );
+            return;
         }
         catch (InterruptedException exception) {
-            System.err.println(this.getClass().getSimpleName() + ": error: update request timeout exceeded");
+            System.err.println(this.getClass().getSimpleName() + ": failed to fetch releases (timed out)");
+            JOptionPane.showMessageDialog(
+                this.gui,
+                "The connection timed out when attempting to fetch release information from GitHub.",
+                "Error",
+                JOptionPane.ERROR_MESSAGE
+            );
+            return;
         }
+
+        // Get the entry for the latest version (i.e. maximum), or null if the map is empty
+        Map.Entry<Version, String> latestVersion = versionsToUrls.entrySet().stream()
+            .max(Map.Entry.comparingByKey())
+            .orElse(null);
+
+        // Get the current application version
+        Version currentVersion = Version.parse(Application.VERSION);
+
+        // Check whether the latest version is "greater than" the current version
+        if (latestVersion != null && latestVersion.getKey().compareTo(currentVersion) > 0) {
+            int choice = JOptionPane.showConfirmDialog(
+                this.gui,
+                "<html>A newer version of " + Application.NAME + " is available for download on GitHub.<ul>"
+                    + "<li>Current version: <b>" + currentVersion + "</b></li>"
+                    + "<li>Latest version: <b>" + latestVersion.getKey() + "</b></li>"
+                    + "</ul>Go to release page?</html>",
+                "Check for Updates",
+                JOptionPane.YES_NO_OPTION
+            );
+
+            if (choice == JOptionPane.YES_OPTION) {
+                // The user has requested to go to the release page, so open it with the default browser
+                try {
+                    Desktop.getDesktop().browse(new URI(latestVersion.getValue()));
+                }
+                catch (Exception exception) {
+                    System.err.println(this.getClass().getSimpleName() + ": failed to browse '" + latestVersion.getValue() + "':");
+                    exception.printStackTrace();
+                    JOptionPane.showMessageDialog(
+                        this.gui,
+                        "<html>Failed to access the following URL:<br><br><a href='"
+                            + latestVersion.getValue() + "'>" + latestVersion.getValue() + "</a></html>",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                    );
+                }
+            }
+        }
+        else {
+            JOptionPane.showMessageDialog(
+                this.gui,
+                "You are using the latest version of " + Application.NAME + " (" + currentVersion + ").",
+                "Check for Updates",
+                JOptionPane.INFORMATION_MESSAGE
+            );
+        }
+    }
+
+    public static Map<Version, String> fetchReleases() throws IOException, InterruptedException {
+        // Create a GitHub API request
+        HttpRequest request;
+        try {
+            request = HttpRequest.newBuilder()
+                .uri(new URI(RELEASES_URL))
+                .timeout(TIMEOUT_DURATION)
+                .GET()
+                .build();
+        }
+        catch (URISyntaxException exception) {
+            // This should not happen unless RELEASES_URL is invalid
+            throw new IllegalStateException(HelpUpdateAction.class.getSimpleName() + ": invalid RELEASES_URL: " + RELEASES_URL);
+        }
+
+        // Send the request
+        HttpResponse<String> response = HttpClient.newHttpClient()
+            .send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Parse the response as JSON
+        JSONArray releases = new JSONArray(response.body());
+
+        // Obtain all versions and corresponding URLs from the JSON array
+        Map<Version, String> versionsToUrls = new HashMap<>();
+        for (int index = 0; index < releases.length(); index++) {
+            JSONObject release = releases.getJSONObject(index);
+            String releaseVersion = release.getString("tag_name");
+            // Remove leading 'v' before tag name, if present
+            if (releaseVersion.startsWith("v")) {
+                releaseVersion = releaseVersion.substring(1);
+            }
+            versionsToUrls.put(Version.parse(releaseVersion), release.getString("html_url"));
+        }
+
+        return versionsToUrls;
     }
 }
