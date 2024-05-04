e5914935749c79b73255a17d68e93b5cbf234426
xarkenz <seanedwardsclarke@gmail.com>
2024-05-04 17:37:41 -0500

    updated version number to 5.0-beta5

--- src/main/java/mars/Application.java
+++ src/main/java/mars/Application.java
@@ -76,7 +76,7 @@ public class Application {
     /**
      * The current MARS Red version number.
      */
-    public static final String VERSION = "5.0-beta4";
+    public static final String VERSION = "5.0-beta5";
     /**
      * List of accepted file extensions for MIPS assembly source files.
      */
