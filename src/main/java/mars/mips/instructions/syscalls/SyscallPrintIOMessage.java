9bd73abaa55cfab2f2773706170a66287b13c07a
Colin Wong <cwongmath@gmail.com>
2024-05-20 04:22:59 -0500

    Add syscall 60 (Print I/O Message)
    May have to be changed to instead just return the string value at an address? Problem is that buffer is not of fixed size, so may have to be allocated in heap. It is also harder to work with than just syscalling once. Perhaps it can just act like `exception.printStackTrace();`? (Maybe it can take a file descriptor to write to, for more versatile logging)

---
+++ src/main/java/mars/mips/instructions/syscalls/SyscallPrintIOMessage.java
@@ -0,0 +1,32 @@
+package mars.mips.instructions.syscalls;
+
+import mars.Application;
+import mars.ProcessingException;
+import mars.ProgramStatement;
+import mars.simulator.Simulator;
+import mars.simulator.SystemIO;
+
+/**
+ * Service to print the last file operation message.
+ */
+public class SyscallPrintIOMessage extends AbstractSyscall {
+    public SyscallPrintIOMessage() {
+        super(60, "PrintIOMessage");
+    }
+
+    /**
+     * Performs syscall function to print the most recent file operation message, as defined
+     * in {@link SystemIO#getFileOperationMessage()}
+     */
+    @Override
+    public void simulate(ProgramStatement statement) throws ProcessingException {
+        String fileOperationMessage = Simulator.getInstance().getSystemIO().getFileOperationMessage();
+        if (Application.getGUI() != null) {
+            Application.getGUI().getMessagesPane().writeToConsole(fileOperationMessage);
+            Application.getGUI().getMessagesPane().writeToConsole("\n");
+        }
+        else {
+            System.out.println(fileOperationMessage);
+        }
+    }
+}
