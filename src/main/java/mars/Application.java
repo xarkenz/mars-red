56f2f6b3203bcb23242fce0182c17b01b1b9d9f7
xarkenz <seanedwardsclarke@gmail.com>
2024-08-23 20:24:08 -0500

    bumped version number to `5.0-beta7`

--- src/main/java/mars/Application.java
+++ src/main/java/mars/Application.java
@@ -55,7 +55,7 @@ public class Application {
     /**
      * The current MARS Red version number.
      */
-    public static final String VERSION = "5.0-beta6";
+    public static final String VERSION = "5.0-beta7";
     /**
      * MARS copyright years.
      */
