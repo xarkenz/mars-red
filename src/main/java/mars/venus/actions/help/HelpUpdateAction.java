de592e15c67cb3ba1ae9a301c77c09c6424a4b41
xarkenz <seanedwardsclarke@gmail.com>
2024-08-25 19:43:57 -0500

    made the Check for Updates action work asynchronously

--- src/main/java/mars/venus/actions/help/HelpUpdateAction.java
+++ src/main/java/mars/venus/actions/help/HelpUpdateAction.java
@@ -20,6 +20,7 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Action for the Help -> Check for Updates menu item.
@@ -36,81 +37,40 @@ public class HelpUpdateAction extends VenusAction {
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        Map<Version, String> versionsToUrls;
-        try {
-            versionsToUrls = fetchReleases();
-        }
-        catch (IOException exception) {
-            System.err.println(this.getClass().getSimpleName() + ": failed to fetch releases:");
-            exception.printStackTrace();
-            JOptionPane.showMessageDialog(
-                this.gui,
-                "Failed to fetch release information from GitHub. (Are you connected to the internet?)",
-                "Error",
-                JOptionPane.ERROR_MESSAGE
-            );
-            return;
-        }
-        catch (InterruptedException exception) {
-            System.err.println(this.getClass().getSimpleName() + ": failed to fetch releases (timed out)");
-            JOptionPane.showMessageDialog(
-                this.gui,
-                "The connection timed out when attempting to fetch release information from GitHub.",
-                "Error",
-                JOptionPane.ERROR_MESSAGE
-            );
-            return;
-        }
-
-        // Get the entry for the latest version (i.e. maximum), or null if the map is empty
-        Map.Entry<Version, String> latestVersion = versionsToUrls.entrySet().stream()
-            .max(Map.Entry.comparingByKey())
-            .orElse(null);
-
-        // Get the current application version
-        Version currentVersion = Version.parse(Application.VERSION);
-
-        // Check whether the latest version is "greater than" the current version
-        if (latestVersion != null && latestVersion.getKey().compareTo(currentVersion) > 0) {
-            int choice = JOptionPane.showConfirmDialog(
-                this.gui,
-                "<html>A newer version of " + Application.NAME + " is available for download on GitHub.<ul>"
-                    + "<li>Current version: <b>" + currentVersion + "</b></li>"
-                    + "<li>Latest version: <b>" + latestVersion.getKey() + "</b></li>"
-                    + "</ul>Go to release page?</html>",
-                "Check for Updates",
-                JOptionPane.YES_NO_OPTION
-            );
-
-            if (choice == JOptionPane.YES_OPTION) {
-                // The user has requested to go to the release page, so open it with the default browser
-                try {
-                    Desktop.getDesktop().browse(new URI(latestVersion.getValue()));
-                }
-                catch (Exception exception) {
-                    System.err.println(this.getClass().getSimpleName() + ": failed to browse '" + latestVersion.getValue() + "':");
-                    exception.printStackTrace();
+        fetchReleases()
+            .thenAccept(this::showUpdateDialog)
+            .exceptionally(exception -> {
+                if (exception instanceof InterruptedException) {
+                    System.err.println(this.getClass().getSimpleName() + ": failed to fetch releases (timed out)");
                     JOptionPane.showMessageDialog(
                         this.gui,
-                        "<html>Failed to access the following URL:<br><br><a href='"
-                            + latestVersion.getValue() + "'>" + latestVersion.getValue() + "</a></html>",
+                        "The connection timed out when attempting to fetch release information from GitHub.",
                         "Error",
                         JOptionPane.ERROR_MESSAGE
                     );
                 }
-            }
-        }
-        else {
-            JOptionPane.showMessageDialog(
-                this.gui,
-                "You are using the latest version of " + Application.NAME + " (" + currentVersion + ").",
-                "Check for Updates",
-                JOptionPane.INFORMATION_MESSAGE
-            );
-        }
+                else {
+                    System.err.println(this.getClass().getSimpleName() + ": failed to fetch releases:");
+                    exception.getCause().printStackTrace(System.err);
+                    JOptionPane.showMessageDialog(
+                        this.gui,
+                        "Failed to fetch release information from GitHub. (Are you connected to the internet?)",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                    );
+                }
+                return null;
+            });
     }
 
-    public static Map<Version, String> fetchReleases() throws IOException, InterruptedException {
+    /**
+     * Asynchronously fetch the list of released versions using the GitHub API.
+     *
+     * @return A mapping from version identifiers to their corresponding user-facing release page URLs,
+     *         wrapped in a {@link CompletableFuture} to enable asynchronous behavior. Note that this future
+     *         completes exceptionally with an {@link IOException} if the HTTP <code>GET</code> request fails.
+     */
+    public static CompletableFuture<Map<Version, String>> fetchReleases() {
         // Create a GitHub API request
         HttpRequest request;
         try {
@@ -125,25 +85,79 @@ public class HelpUpdateAction extends VenusAction {
             throw new IllegalStateException(HelpUpdateAction.class.getSimpleName() + ": invalid RELEASES_URL: " + RELEASES_URL);
         }
 
-        // Send the request
-        HttpResponse<String> response = HttpClient.newHttpClient()
-            .send(request, HttpResponse.BodyHandlers.ofString());
+        return HttpClient.newHttpClient()
+            // Send the request asynchronously
+            .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+            // Process the response once it is received
+            .thenApply((response) -> {
+                // Parse the response as JSON
+                JSONArray releases = new JSONArray(response.body());
 
-        // Parse the response as JSON
-        JSONArray releases = new JSONArray(response.body());
+                // Obtain all versions and corresponding URLs from the JSON array
+                Map<Version, String> versionsToUrls = new HashMap<>();
+                for (int index = 0; index < releases.length(); index++) {
+                    JSONObject release = releases.getJSONObject(index);
+                    String releaseVersion = release.getString("tag_name");
+                    // Remove leading 'v' before tag name, if present
+                    if (releaseVersion.startsWith("v")) {
+                        releaseVersion = releaseVersion.substring(1);
+                    }
+                    versionsToUrls.put(Version.parse(releaseVersion), release.getString("html_url"));
+                }
 
-        // Obtain all versions and corresponding URLs from the JSON array
-        Map<Version, String> versionsToUrls = new HashMap<>();
-        for (int index = 0; index < releases.length(); index++) {
-            JSONObject release = releases.getJSONObject(index);
-            String releaseVersion = release.getString("tag_name");
-            // Remove leading 'v' before tag name, if present
-            if (releaseVersion.startsWith("v")) {
-                releaseVersion = releaseVersion.substring(1);
+                return versionsToUrls;
+            });
+    }
+
+    private void showUpdateDialog(Map<Version, String> versionsToUrls) {
+        // Run on the GUI thread
+        SwingUtilities.invokeLater(() -> {
+            // Get the entry for the latest version (i.e. maximum), or null if the map is empty
+            Map.Entry<Version, String> latestVersion = versionsToUrls.entrySet().stream()
+                .max(Map.Entry.comparingByKey())
+                .orElse(null);
+
+            // Get the current application version
+            Version currentVersion = Version.parse(Application.VERSION);
+
+            // Check whether the latest version is "greater than" the current version
+            if (latestVersion != null && latestVersion.getKey().compareTo(currentVersion) > 0) {
+                int choice = JOptionPane.showConfirmDialog(
+                    this.gui,
+                    "<html>A newer version of " + Application.NAME + " is available for download on GitHub.<ul>"
+                    + "<li>Current version: <b>" + currentVersion + "</b></li>"
+                    + "<li>Latest version: <b>" + latestVersion.getKey() + "</b></li>"
+                    + "</ul>Go to release page?</html>",
+                    "Check for Updates",
+                    JOptionPane.YES_NO_OPTION
+                );
+
+                if (choice == JOptionPane.YES_OPTION) {
+                    // The user has requested to go to the release page, so open it with the default browser
+                    try {
+                        Desktop.getDesktop().browse(new URI(latestVersion.getValue()));
+                    }
+                    catch (Exception exception) {
+                        System.err.println(this.getClass().getSimpleName() + ": failed to browse '" + latestVersion.getValue() + "':");
+                        exception.printStackTrace(System.err);
+                        JOptionPane.showMessageDialog(
+                            this.gui,
+                            "<html>Failed to access the following URL:<br><br><a href='"
+                            + latestVersion.getValue() + "'>" + latestVersion.getValue() + "</a></html>",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE
+                        );
+                    }
+                }
             }
-            versionsToUrls.put(Version.parse(releaseVersion), release.getString("html_url"));
-        }
-
-        return versionsToUrls;
+            else {
+                JOptionPane.showMessageDialog(
+                    this.gui,
+                    "You are using the latest version of " + Application.NAME + " (" + currentVersion + ").",
+                    "Check for Updates",
+                    JOptionPane.INFORMATION_MESSAGE
+                );
+            }
+        });
     }
 }
